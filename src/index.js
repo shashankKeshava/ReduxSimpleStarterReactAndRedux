f5534f0880887f84a800b2ca7439f1067ce356a2
Shashank <keshavashashank@gmail.com>
2017-12-14 08:58:35 +0530

    Lecture 48

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import './ReactotronConfig'
 
 import App from './components/app';
 import reducers from './reducers';
